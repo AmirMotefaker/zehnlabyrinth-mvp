2ee1550ae81c75f85fb647745cf7b9af95f48628
Amir Motefaker <amotef@gmail.com>
2026-09-04 12:06:14 +0330

    fix(phaser): remove deprecated Capacitor config option

--- phaser/capacitor.config.ts
+++ phaser/capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'ir.amirmotefaker.neyro',
   appName: 'NEYRO',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist'
 }
 
 export default config
